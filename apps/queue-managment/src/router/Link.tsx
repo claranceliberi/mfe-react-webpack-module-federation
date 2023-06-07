76910cd7e975319f89736bce1f8e91381b59753a
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-06-07 10:28:32 +0200

    feat: proper routing

--- apps/queue-managment/src/router/Link.tsx
+++ apps/queue-managment/src/router/Link.tsx
@@ -5,5 +5,5 @@ import { urlLinker } from 'shared-util';
 //this component plays a crucial role in the routing of the application, by allowing you to navigate in the app
 // without having to specify the main app path
 export default function Link({ to, ...props }: LinkProps & React.RefAttributes<HTMLAnchorElement>) {
-  return <CLink to={urlLinker('examination', to)} {...props} />;
+  return <CLink to={urlLinker('queue-management', to)} {...props} />;
 }
